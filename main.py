b99d74d76c92926189c711350114c0675e4fc3c2
lukasvdberk <lukasvdberk@gmail.com>
2025-12-20 13:53:31 +0100

    cleaned up some example code, added librespot bin

--- main.py
+++ main.py
@@ -7,28 +7,29 @@ import subprocess
 import decky
 import asyncio
 
+# TODO move to a the configurable settings manager mentioned in the docs
+LIBRESPOT_BIN = "/home/deck/librespot"
+LIBRESPOT_SPEAKER_NAME="decky"
 class Plugin:
-    # A normal method. It can be called from the TypeScript side using @decky/api.
-    async def add(self, left: int, right: int) -> int:
-        return left + right
-
     async def long_running(self):
-        await asyncio.sleep(15)
         # Passing through a bunch of random data, just as an example
         await decky.emit("timer_event", "Hello from the backend!", True, 2)
+
+        decky.logger.info("Attempting to start librespot")
+        # start speaker in background
+        subprocess.Popen([LIBRESPOT_BIN, "-n", LIBRESPOT_SPEAKER_NAME, "-b", "320"]) 
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
         self.loop = asyncio.get_event_loop()
-        decky.logger.info("Hello World!")
-
-        # start speaker in background
-        subprocess.Popen(["/home/lukas/.cargo/bin/librespot", "-n", "Librespot Speaker", "-b", "320"]) 
+        decky.logger.info("Starting librespot plugin")
 
     # Function called first during the unload process, utilize this to handle your plugin being stopped, but not
     # completely removed
     async def _unload(self):
         decky.logger.info("Goodnight World!")
+
+        # TODO add the stopping of librespot
         pass
 
     # Function called after `_unload` during uninstall, utilize this to clean up processes and other remnants of your
@@ -37,25 +38,5 @@ class Plugin:
         decky.logger.info("Goodbye World!")
         pass
 
-    async def start_timer(self):
-        self.loop.create_task(self.long_running())
-
-    # Migrations that should be performed before entering `_main()`.
     async def _migration(self):
-        decky.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky.decky_LOG_DIR/template.log`
-        decky.migrate_logs(os.path.join(decky.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky.decky_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky.decky_SETTINGS_DIR/`
-        decky.migrate_settings(
-            os.path.join(decky.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-        decky.migrate_runtime(
-            os.path.join(decky.DECKY_HOME, "template"),
-            os.path.join(decky.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        pass
